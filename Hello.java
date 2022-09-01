d00a3ba1f27f3cbcc797ac76ef39712587b18e37
ParvezHossain <parvezhossain724@gmail.com>
2022-09-02 02:39:59 +0600

    Hello.java filr  has been modified

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello From Java");
+        System.out.println("Hello From Java, and we are doing ci/cd using jenkins");
     }
 
 }
